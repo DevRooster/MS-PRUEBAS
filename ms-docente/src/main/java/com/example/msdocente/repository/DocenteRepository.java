ab2b23bd5794b1a4f97de0f02dd9a76cbdc13abd
EnriqueMV18 <enriquemv937@gmail.com>
2024-04-21 09:18:09 -0500

    Agregar configuracion

--- ms-docente/src/main/java/com/example/msdocente/repository/DocenteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.msdocente.repository;
-
-import com.example.msdocente.entity.Docente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DocenteRepository extends JpaRepository<Docente,Integer> {
-}
