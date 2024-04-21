debb724e4c981e9f9ce8ac5c5d2c1abb064d0c6c
EnriqueMV18 <enriquemv937@gmail.com>
2024-04-21 10:43:03 -0500

    ms-admatricula

---
+++ ms-admatricula/src/main/java/com/example/msadmatricula/MsAdmatriculaApplication.java
@@ -0,0 +1,13 @@
+package com.example.msadmatricula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAdmatriculaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAdmatriculaApplication.class, args);
+	}
+
+}
